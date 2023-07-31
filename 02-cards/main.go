65ba86362f735d265caaf544bdf498853ed15a8b
Ajmera Pavan Kumar <ajmerapavan20@gmail.com>
2023-07-31 23:42:14 +0530

    Go Basics

--- 02-cards/main.go
+++ 02-cards/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Cards")
+
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	fmt.Println("Cards: ", cards)
+
+	for i, card := range cards {
+		fmt.Print(i, card, "\n")
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
